520790183fb7e5398a35113097d85385588a57c0
ko5cles <939588715@qq.com>
2023-07-24 09:38:34 -0400

    add capybara

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+$(document).ready(function () {
+    main();
+})
+
+function MoveUp(elem,height){
+    let pos=-height;
+    let timer = setInterval(function () {
+        pos++;
+        elem.style.bottom = pos + "px";
+        if (pos === 0) clearInterval(timer);
+    }, 25);
+}
+function MoveDown(elem,height){
+    let pos=0;
+    let timer = setInterval(function () {
+        pos--;
+        elem.style.bottom = pos + "px";
+        if (pos === -height) clearInterval(timer);
+    }, 25);
+}
+function main(){
+    let elem=document.getElementById("capybara");
+    MoveUp(elem,160);
+    elem.addEventListener("click", function (){MoveDown(elem,160);});
+}
