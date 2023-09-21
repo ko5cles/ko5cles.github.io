56d8ef649433d3db7e8a91038912d7c508f17128
ko5cles <939588715@qq.com>
2023-09-21 15:32:28 -0400

    add support for mobile navbar

--- script.js
+++ script.js
@@ -3,24 +3,59 @@ $(document).ready(function () {
     main();
 })
 
-function MoveUp(elem,height){
-    let pos=-height;
+function Rotate(elem, start, end) {
+    if (start < end) {
+        let timer = setInterval(function () {
+            start++;
+            elem.css("rotate", start + "deg");
+            if (start === end) clearInterval(timer);
+        })
+    } else {
+        let timer = setInterval(function () {
+            start--;
+            elem.css("rotate", start + "deg");
+            if (start === end) clearInterval(timer);
+        })
+    }
+}
+
+function CallMenu() {
+    let menu = $(".mobile-navbar .menu");
+    let img = $("#hamburger img");
+    let bar = $(".mobile-navbar");
+    if (menu.css("display") === "none") {
+        Rotate(img, 0, 90);
+        menu.css("display", "grid");
+        bar.css({"border": "gray 2px solid", "border-radius": "5px"});
+    } else {
+        Rotate(img, 90, 0);
+        menu.css("display", "none");
+        bar.css({"border": "none", "border-radius": "none"});
+    }
+}
+
+function MoveUp(elem, height) {
+    let pos = -height;
     let timer = setInterval(function () {
         pos++;
         elem.style.bottom = pos + "px";
         if (pos === 0) clearInterval(timer);
     }, 25);
 }
-function MoveDown(elem,height){
-    let pos=0;
+
+function MoveDown(elem, height) {
+    let pos = 0;
     let timer = setInterval(function () {
         pos--;
         elem.style.bottom = pos + "px";
         if (pos === -height) clearInterval(timer);
     }, 25);
 }
-function main(){
-    let elem=document.getElementById("capybara");
-    MoveUp(elem,160);
-    elem.addEventListener("click", function (){MoveDown(elem,160);});
+
+function main() {
+    let elem = document.getElementById("capybara");
+    MoveUp(elem, 160);
+    elem.addEventListener("click", function () {
+        MoveDown(elem, 160);
+    });
 }
